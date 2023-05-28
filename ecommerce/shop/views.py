9fa32f2a5a32bc96f3fd8053872bba2e29a4d1a8
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-28 21:08:08 +0300

    Le logique de recherche

--- ecommerce/shop/views.py
+++ ecommerce/shop/views.py
@@ -3,8 +3,14 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
+
+
 def index(request):
     product_object = Product.objects.all()
+    item_name = request.GET.get('item-name')
+    if (item_name != "" and item_name is not None):
+        product_object = Product.objects.filter(title__icontains=item_name)
+
     context = {
         'products': product_object
     }
