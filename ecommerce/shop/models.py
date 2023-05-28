897955e2520c76fcc6d078beb880b2ae6fccef5b
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-28 19:33:04 +0300

    Retour __str__

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -9,6 +9,9 @@ class Category(models.Model):
 
     class Meta:
         ordering = ['-date_added']
+    
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
@@ -22,3 +25,6 @@ class Product(models.Model):
 
     class Meta:
         ordering = ['-date_added']
+    
+    def __str__(self):
+        return self.title
