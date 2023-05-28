bb737d2661e30f6b532a1cb02b31b83511d22908
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-28 19:19:43 +0300

    Ajout de models

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, related_name='categorie', on_delete=models.CASCADE)
+    image = models.CharField(max_length=5000)
+    date_added = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-date_added']
+
