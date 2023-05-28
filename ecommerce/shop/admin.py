37f34188860a320298e7869ac82234edd131611a
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-28 19:41:37 +0300

    Mise en place de base dans le template

--- ecommerce/shop/admin.py
+++ ecommerce/shop/admin.py
@@ -7,7 +7,7 @@ from .models import Category, Product
 
 class AdminCategorie(admin.ModelAdmin):
     list_display = ('name', 'date_added')
-
+    
 
 class AdminProduct(admin.ModelAdmin):
     list_display = ('title', 'price', 'category', 'date_added')
