c241d3f566f3d6d6a01bafedcdb0addf75995a3d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-29 06:01:13 +0300

    Ajout de code de checkout

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from shop.views import index,detail
+from shop.views import index, detail, checkout
 
 urlpatterns = [
     path('', index, name='home'),
-    path('<int:myid>', detail, name='detail')
+    path('<int:myid>', detail, name='detail'),
+    path('checkout', checkout, name='checkout')
 ]
